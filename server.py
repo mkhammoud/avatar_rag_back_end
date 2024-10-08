8b80dcf0e76385eb9bceaf1949b45dd4a43d39a1
mkhammoud <moh.k.hammoud@gmail.com>
2024-10-08 16:23:28 +0300

    basic route testing and names

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 
+import json
 from flask import Flask, jsonify, send_file,request
 from flask_cors import CORS, cross_origin
 
 app = Flask("Avatar RAG Backend")
 app_url="http://localhost:5000/"
 CORS(app, origins=['*'])
+
+
+# GET IDLE AVATAR FOR LOCAL VIDEO 
 
 @app.route('/getIdleAvatar', methods=["POST","OPTIONS"])
 @cross_origin(supports_credentials=True)
@@ -13,14 +17,15 @@ def get_idle_avatar_route():
         if "avatarId" in request.form:
             
             avatarId=request.form["avatarId"]
+            avatarProvider=request.form["avatarProvider"]
 
             # Simulate Text to speech avatar pipeline or so 
 
-            video_path="idle_video.mp4"
+            video_path="default_avatar.mp4"
 
             return jsonify({
             'status':"success",
-            'video_url':f'{app_url}video/'+video_path
+            'video_url':f'{app_url}video/'+video_path,
             })      
 
 
@@ -32,8 +37,13 @@ def get_idle_avatar_route():
 @cross_origin(supports_credentials=True)
 def handle_user_query_route():
     try:
-            
-        userTextInput=request.form["userTextInput"]
+
+        if "messages" in request.form:
+            messages_str = request.form['messages']
+            messages=None
+            if messages_str:
+                messages = json.loads(messages_str)  # Convert JSON string back to array
+                print(messages)
 
         # Simulate Search Call (you have to embed the user query in your search pipeline and then perform RAG)
 
@@ -44,8 +54,8 @@ def handle_user_query_route():
         # Return video
 
         video_path="result_video.mp4"
-        print(video_path)
-        return jsonify({'status':"success",'video_url':f'{app_url}video/'+video_path})
+        return jsonify({'status':"success",'video_url':f'{app_url}video/'+video_path,'text_response':"Hello! How can I assist you today? Feel free to ask me anything, whether it's about general knowledge, recommendations, or troubleshooting. I'm here to help!"})
+
 
     except Exception as e:
         print(e)
